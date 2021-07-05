0d3d89a8400befdce5ed7128fdcc60a821ef133b
mo1ein <moeinn.com@gmail.com>
2021-07-05 20:10:18 +0430

    Clean code 😌

--- Algorithms/Problems/Pow.cpp
+++ Algorithms/Problems/Pow.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 
-int pow(int &base, int &exponent)
+int Power(int base, int exponent)
 {
     if(exponent == 0)
         return 1;
     else if(exponent % 2 == 0)
-        return pow(base * base, exponent / 2);
+        return Power(base * base, exponent / 2);
     else
-        return base * pow(base * base, (exponent - 1) / 2);
+        return base * Power(base * base, (exponent - 1) / 2);
 }
 
 
@@ -17,5 +17,5 @@ int main()
 {
     int base, exponent;
     cin >> base >> exponent;
-    cout << pow(base, exponent);
+    cout << Power(base, exponent);
 }
