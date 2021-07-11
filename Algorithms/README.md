0bc4b4117ef3edd762b867488d59e02356910a2b
mo1ein <moeinn.com@gmail.com>
2021-07-11 17:55:35 +0430

    Add algorithm 🤘

--- Algorithms/README.md
+++ Algorithms/README.md
@@ -14,6 +14,11 @@
 ## Famous algorithms
 ### Binary search
 <ul>
-<li>[iterative binary search](./iterative_binary_search.cpp)</li>
-<li>[recursive binary search](./recursive_binary_search.cpp)</li>
+<li>[iterative binary search](./binary\ search/iterative_binary_search.cpp)</li>
+<li>[recursive binary search](./binary\ search/recursive_binary_search.cpp)</li>
+</ul>
+
+### Sorting Algorithms
+<ul>
+<li>[insertion sort](./sorting algorithms/insertion sort/insertion_sort.cpp)</li>
 </ul>
---
+++ Algorithms/sorting algorithms/insertion sort/insertion_sort_2.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    vector<int> arr;
+    arr = {1, 6, 5, 0, 23, 11, 9};
+
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+
+    cout << "\nafter sorting....\n";
+
+    for (int i = 1; i < arr.size(); i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            // using swap
+            swap(arr[j + 1], arr[j]);
+            j--;
+        }
+    }
+
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+}
